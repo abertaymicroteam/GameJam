ce40cbcbc8267e2d16d3a368f4f260c11e264f23
cimcpherson <1401373@uad.ac.uk>
2017-01-21 01:22:28 +0000

    commit

---
+++ PoolParty/Assets/KnobMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnobMovement : MonoBehaviour {
+
+	private Rigidbody rigBody;
+	private GameObject dude;
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigBody = gameObject.GetComponent<Rigidbody> ();
+		dude = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			Vector3 mousePos = Input.mousePosition;
+			Vector3 playerPos = camera.WorldToScreenPoint(dude.transform.position);
+
+			Vector3 distance = playerPos - mousePos;
+
+			if (distance.magnitude > 0) 
+			{
+				distance.Normalize();
+
+				rigBody.AddForce (distance * 2.0f, ForceMode.Impulse);
+			}
+		}
+	}
+}
